1125742f59f853018992bfb6e1662049d26f1f48
David Wieler <davidw@focuswebmedia.com>
2023-04-18 23:30:46 -0600

    full deployment test

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -3,6 +3,10 @@ echo 'run after_install.sh: ' >> /home/ec2-user/test/deploy.log
 
 echo 'cd /home/ec2-user/test' >> /home/ec2-user/test/deploy.log
 cd /home/ec2-user/test >> /home/ec2-user/test/deploy.log
+npm install >> /home/ec2-user/test/deploy.log
+npm run build >> /home/ec2-user/test/deploy.log
+
+pm2 restart nextjs
 
 # echo 'npm install' >> /home/ec2-user/test/deploy.log 
 # npm install >> /home/ec2-user/test/deploy.log
