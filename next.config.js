96ebf47e31337b6f63d10a919d4f3a733ccbce60
David Wieler <davidw@focuswebmedia.com>
2023-04-19 08:32:54 -0600

    Update github action to run linter

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    // Linting will happen at the Github workflow step, not on builds.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
