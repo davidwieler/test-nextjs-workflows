14046f729d78ff2e031b1c245a39ee6bf19de7f2
David Wieler <davidw@focuswebmedia.com>
2023-04-19 08:47:16 -0600

    update checkout to v3

--- next.config.js
+++ next.config.js
@@ -3,10 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+    // the project has ESLint errors.
     // Linting will happen at the Github workflow step, not on builds.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // the project has TS Errors errors.
+    // TS check will happen at the Github workflow step, not on builds.
+    ignoreDuringBuilds: true
+  }
 }
 
 module.exports = nextConfig
