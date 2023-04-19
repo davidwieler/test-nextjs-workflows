86d82b77a8906669e6b7e888effbf67a069323cc
David Wieler <davidw@focuswebmedia.com>
2023-04-18 20:32:01 -0600

    deploy test

---
+++ scripts/application-start.sh
@@ -0,0 +1,6 @@
+# #!/bin/bash
+
+# echo 'run application_start.sh: ' >> /home/ec2-user/test/deploy.log
+
+# echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/test/deploy.log
+# pm2 restart nodejs-express-app >> /home/ec2-user/test/deploy.log
