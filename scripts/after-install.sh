3c6ca054b114c2d7db70de4d369cc653fb6f4aee
David Wieler <davidw@focuswebmedia.com>
2023-04-19 09:03:34 -0600

    update pm2 restart script

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -3,10 +3,12 @@ echo 'run after_install.sh: ' >> /home/ec2-user/test/deploy.log
 
 echo 'cd /home/ec2-user/test' >> /home/ec2-user/test/deploy.log
 cd /home/ec2-user/test >> /home/ec2-user/test/deploy.log
+echo 'starting NPM install...' >> /home/ec2-user/test/deploy.log
 npm install >> /home/ec2-user/test/deploy.log
+echo 'starting build...' >> /home/ec2-user/test/deploy.log
 npm run build >> /home/ec2-user/test/deploy.log
-
-pm2 restart nextjs >> /home/ec2-user/test/deploy.log
+echo 'restarting PM2 services' >> /home/ec2-user/test/deploy.log
+# pm2 restart all >> /home/ec2-user/test/deploy.log
 
 # echo 'npm install' >> /home/ec2-user/test/deploy.log 
 # npm install >> /home/ec2-user/test/deploy.log
