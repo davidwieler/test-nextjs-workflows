419d4cb637011bc9be551dcf8b753e8d328f0f49
David Wieler <davidw@focuswebmedia.com>
2023-04-19 08:55:27 -0600

    remove wrong config option

--- next.config.js
+++ next.config.js
@@ -6,12 +6,6 @@ const nextConfig = {
     // the project has ESLint errors.
     // Linting will happen at the Github workflow step, not on builds.
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // the project has TS Errors errors.
-    // TS check will happen at the Github workflow step, not on builds.
-    ignoreDuringBuilds: true
   }
 }
 
