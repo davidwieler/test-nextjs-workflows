add85fb300902655835d9e6c7933b5a6d955e7f7
David Wieler <davidw@focuswebmedia.com>
2023-04-21 10:27:36 -0600

    Add prop tests

--- pages/health.tsx
+++ pages/health.tsx
@@ -1,3 +1,12 @@
-export default function Health() {
-    return <div>up</div>
-  }
+export const getServerSideProps = () => {
+  return {
+    props: {
+      textData: "example2",
+    },
+  };
+};
+
+export default function Health(props) {
+  console.log("=== props health.tsx [11] ===", props);
+  return <div>up</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,16 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export const getStaticProps = () => {
+  return {
+    props: {
+      textData: 'example'
+    }
+  }
+};
+
+export default function Home(props) {
+  console.log(props);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
